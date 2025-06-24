da310a6c89c973c01246fa54177459825f87ccc2
Arief <arief.d2202@gmail.com>
2025-06-24 21:46:55 +0700

    Pedoman Pemeriksaan Kesehatan Done

--- app/Http/Controllers/PedomanPemeriksaanKesehatanController.php
+++ app/Http/Controllers/PedomanPemeriksaanKesehatanController.php
@@ -10,6 +10,7 @@ use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File;
 
 class PedomanPemeriksaanKesehatanController extends Controller
 {
@@ -18,9 +19,8 @@ class PedomanPemeriksaanKesehatanController extends Controller
      */
     public function index()
     {
-        if(Auth::user()->role_id != 1) return redirect('/');
         return view('PedomanPemeriksaanKesehatan', [
-            'datas' => User::all(),
+            'data' => PedomanPemeriksaanKesehatan::first(),
         ]);
     }
 
@@ -29,33 +29,23 @@ class PedomanPemeriksaanKesehatanController extends Controller
      */
     public function create(Request $request)
     {
-        User::create([
-            'name' => $request->name,
-            'role_id' => $request->role,
-            'nip' => $request->nip,
-            'password' => Hash::make($request->password),
-        ]);
-        return redirect('/users');
-    }
-    public function edit(Request $request)
-    {
-        $user = User::where('id', "=", $request->id)->first();
-        if(isset($request->name)) $user->name = $request->name;
-        if(isset($request->role)) $user->role_id = $request->role;
-        if(isset($request->password)) $user->password = Hash::make($request->password);
-        $user->save();
-        return redirect('/users');
-    }
-    public function delete(Request $request)
-    {
-        $user = User::where('id', "=", $request->id)->first();
-        if($user) $user->delete();
-        return redirect('/users');
-    }
-    public function getById(Request $request)
-    {
-        $user = User::where('id', "=", $request->id)->first();
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($user);die;
+        $destinationPath = 'uploads/SMK3/pemeriksaan-kesehatan-pekerja/pedoman-pemeriksaan-kesehatan';
+        $fileName = date("YmdHis").'.'.$request->file->extension();
+        $request->file->move(public_path($destinationPath), $fileName);
+        $datas = PedomanPemeriksaanKesehatan::first();
+        if($datas){
+            File::delete(public_path().$datas->dokumen_pedoman);
+            $datas->dokumen_pedoman = '/'.$destinationPath.'/'.$fileName;
+            $datas->editor_id = Auth::user()->id;
+            $datas->save();
+        }
+        else{
+            PedomanPemeriksaanKesehatan::create([
+                'id' => 1,
+                'dokumen_pedoman' => '/'.$destinationPath.'/'.$fileName,
+                'editor_id' => Auth::user()->id,
+            ]);
+        }
+        return redirect('/smk3/pemeriksaan-kesehatan-pekerja/pedoman-pemeriksaan-kesehatan');
     }
 }
--- resources/views/PedomanPemeriksaanKesehatan.blade.php
+++ resources/views/PedomanPemeriksaanKesehatan.blade.php
@@ -7,65 +7,42 @@
             <div class="alert-danger mt-1 p-2">{{ $errorMessage }}</div>
         @endif
 
-        <div class="row mt-2">
+        <div class="row mt-2 mb-3">
           <div class="col-6">
             <div class="col">
               <h5 class="card-title">Pedoman Pemeriksaan Kesehatan</h5>
             </div>
           </div>
+        @if(Auth::user()->role_id == 3)
           <div class="col-6 d-flex justify-content-end h-50">
             <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Tambahkan Data</button>
           </div>
+        @endif
         </div>
-
-        <div style="max-height: 100vh; overflow-y:auto;">
-            <div class="card-text me-3">
-                  <div style="max-height: 68vh; overflow-y:auto;">
-                    <div class="card-text me-3">
-
-                      <table id="myTable">
-                        <thead class="thead">
-                            <tr>
-                                <th>Column 1</th>
-                                <th>Column 2</th>
-                                <th>Column 3</th>
-                                <th>Column 4</th>
-                                <th>Column 5</th>
-                            </tr>
-                        </thead>
-                        <tbody class="tbody">
-                            <?php for($a =0; $a<50; $a++){?>
-                            <tr>
-                                <td>Row {{$a}} Data 1</td>
-                                <td>Row {{$a}} Data 2</td>
-                                <td>Row {{$a}} Data 3</td>
-                                <td>Row {{$a}} Data 4</td>
-                                <td>Row {{$a}} Data 5</td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                      </table>
-
-                    </div>
-                  </div>
-            </div>
+        <div class="d-flex justify-content-center">
+            @if($data)<img src="{{$data->dokumen_pedoman}}" alt="" style="max-height: 60vh; max-width: 80%;">@endif
         </div>
 
 
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                    <form method="POST" enctype="multipart/form-data">@csrf
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Tambahkan Data</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="formFile" class="form-label">Dokumen Pedoman</label>
+                                <input class="form-control" type="file" id="formFile" name="file" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
